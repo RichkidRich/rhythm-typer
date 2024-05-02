877866e82d1a4f4be371a0845084cf246ce04b1a
RichkidRich <50368894+RichkidRich@users.noreply.github.com>
2024-05-02 15:31:33 +0900

    Print statements in time to taiko chart

--- src/App.js
+++ src/App.js
@@ -1,23 +1,145 @@
 import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+import { motion } from "framer-motion";
+import { generate } from 'random-words';
+import { Container, Row, Col } from 'react-bootstrap';
+import { useState, useEffect } from 'react';
+import { useTimer } from "react-use-precision-timer";
+import processSong from './songProcessor';
+
+const BEAT_INTERVAL = 48;
 
 function App() {
+
+  const [targetParagraph, setTargetParagraph] = useState("");
+  const [wordDifficulty, setWordDifficulty] = useState(7);
+  const [songProcessing, setSongProcessing] = useState(false);
+  const [songProcessed, setSongProcessed] = useState(false);
+  const [songData, setSongData] = useState(null);
+
+  const [songAudio, setSongAudio] = useState(null);
+  const [beatAudio, setBeatAudio] = useState(null);
+
+  const [readInputEnabled, setReadInputEnabled] = useState(false);
+
+  const [songStarted, setSongStarted] = useState(false);
+  const [currentFrame, setCurrentFrame] = useState(0);
+  const [currentFrameIndex, setCurrentFrameIndex] = useState(0);
+  const [beatCounter, setBeatCounter] = useState(0);
+
+  const handleNewBeat = () => {
+    // console.log(`count: ${beatCounter}`);
+    if (songStarted) {
+      const frameData = songData.noteData[currentFrame];
+      const frameIndex = currentFrameIndex;
+      const frameLength = frameData.length;
+
+      const beatsPerIndex = BEAT_INTERVAL / frameLength;
+
+      // console.log(frameData[frameIndex]);
+
+      if (beatCounter === 0 && parseInt(frameData[frameIndex]) > 0 && parseInt(frameData[frameIndex]) < 8){
+        console.log(`play ${frameData[frameIndex]}`);
+      }
+
+      if ((frameIndex + 1 === frameLength) && (beatCounter + 1 === beatsPerIndex)) {
+        setCurrentFrameIndex(0);
+        setCurrentFrame(currentFrame + 1);
+      } else {
+        if (!(frameIndex + 1 === frameLength) && (beatCounter + 1 === beatsPerIndex)) {
+          setCurrentFrameIndex(frameIndex + 1);
+        }
+      }
+
+      if (beatCounter + 1 === beatsPerIndex) {
+        setBeatCounter(0);
+      } else {
+        setBeatCounter(beatCounter + 1);
+      }
+
+    }
+  }
+
+  const beatTimer = useTimer({
+    delay: (60000 * 1 * 4 / (songData ? songData.bpm : 180))/BEAT_INTERVAL},
+    handleNewBeat
+  );
+
+  const generateTargetParagraph = (numWords) => {
+    const text = generate({ minLength: 3, maxLength: wordDifficulty, join: ' ', exactly: numWords });
+  };
+
+  const loadSong = async (setSongProcessed, setSongData) => {
+    const tjaPath = "/Songs/Night of Knights/Night of Knights.tja";
+    await processSong(tjaPath, setSongProcessed, setSongData);
+  }
+
+  const startSong = async () => {
+    if (songAudio) {
+      setReadInputEnabled(true);
+      songAudio.currentTime = Math.abs(songData.offset);
+      setInterval(() => {
+        songAudio.play();
+        setSongStarted(true);
+      }, (60000 * 1 * 4 / (songData ? songData.bpm : 180)))
+      startMetronome();
+    } else {
+      console.log('Song audio not loaded');
+    }
+  }
+
+  const startMetronome = async () => {
+    beatTimer.start();
+  }
+
+  useEffect(() => {
+    setSongProcessing(true);
+    loadSong(setSongProcessed, setSongData);
+  }, []);
+
+  useEffect(() => {
+    if (songProcessed) {
+      console.log('Processed Song Data:');
+      console.log(songData);
+      setSongAudio(new Audio(process.env.PUBLIC_URL + songData.songSource));
+    }
+  }, [songProcessed]);
+
+  useEffect(() => {
+    if (songAudio) {
+      songAudio.load();
+    }
+  
+  }, [songAudio]);
+
+  const handleKeyDown = (event) => {
+    if (readInputEnabled) {
+      console.log(event.key);
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="main-background" onKeyDown={handleKeyDown} tabIndex="0">
+      <Container>
+        <Row>
+            <p className='main-header'>
+              Rhythm Typer
+            </p>
+        </Row>
+        <Row className="sub-header">
+          <p>
+            Where every round is a sight read round
+          </p>
+          <p>
+            {"> Pick a song and type to the beat <"}
+          </p>
+        </Row>
+        <Row className="play-row">
+          <div className="play-button" onClick={startSong}>PLAY</div>
+        </Row>
+      </Container>
     </div>
   );
 }
---
+++ src/songProcessor.js
@@ -0,0 +1,157 @@
+async function readContentFromFile(filePath) {
+    try {
+        const response = await fetch(process.env.PUBLIC_URL + filePath);
+        if (!response.ok) {
+            throw new Error(`HTTP error! status: ${response.status}`);
+        }
+        const data = await response.text();
+        return data;
+    } catch (err) {
+        console.error(`Error reading file from disk: ${err}`);
+    }
+};
+
+const processSong = async (tjaFile, setSongProcessed, setSongData) => {
+    console.log('Processing song...');
+
+    const fileContent = await readContentFromFile(tjaFile);
+    const fileLines = fileContent.split('\n');
+
+    // console.log(fileLines);
+    // navigator.clipboard.writeText(fileLines);
+
+    let title = null;
+    let subtitle = null;
+    let bpm = null;
+    let songSource = null;
+    let offset = null;
+    let demoStart = null;
+
+    let difficulty = null;
+    let balloons = [];
+    let scoreInit = null;
+    let scoreDiff = null;
+
+    let beginChartProcess = false;
+    let noteDataBegins = false;
+
+    let extract = true;
+
+    const noteDataArray = [];
+
+    for (let i = 0; i < fileLines.length; i++) {
+        const line = fileLines[i].split("/")[0];
+
+        if (line.trim() === '') {
+            continue;
+        }
+
+        if (!title) {
+            if (line.includes('TITLE:')) {
+                title = line.split(':')[1].trim();
+                continue;
+            }
+        }
+
+        if (!subtitle) {
+            if (line.includes('SUBTITLE:')) {
+                subtitle = line.split(':')[1].trim();
+                continue;
+            }
+        }
+
+        if (!bpm) {
+            if (line.includes('BPM:')) {
+                bpm = line.split(':')[1].trim();
+                continue;
+            }
+        }
+
+        if (!offset) {
+            if (line.includes('OFFSET:')) {
+                offset = line.split(':')[1].trim();
+                continue;
+            }
+        }
+
+        if (!demoStart) {
+            if (line.includes('DEMOSTART:')) {
+                demoStart = line.split(':')[1].trim();
+                continue;
+            }
+        }
+
+        if (!songSource) {
+            if (line.includes('WAVE:')) {
+                const songFile = line.split(':')[1].trim();
+                songSource = `/Songs/${songFile.split('.')[0]}/${songFile}`;
+                continue;
+            }
+        }
+        if (!beginChartProcess) {
+            if (line.includes('COURSE:') && line.toUpperCase().includes('HARD')) {
+                beginChartProcess = true;
+                continue;
+            }
+        } else {
+            // process song chart data
+            if (line.includes('#END')) {
+                break;
+            }
+
+            if (noteDataBegins) {
+                if (line.includes('#')) {
+                    if (!extract && line.includes('#M')) {
+                        extract = true;
+                    }
+                    if (line.includes('BRANCHSTART')) {
+                        extract = false;
+                    }
+                    continue;
+                }
+
+                if (extract) {
+                    noteDataArray.push(line.split(',')[0]);
+                }
+            }
+
+            if (line.includes('#START')) {
+                noteDataBegins = true;
+                continue;
+            }
+            
+            if (line.includes('LEVEL:')) {
+                difficulty = line.split(':')[1].trim();
+                continue;
+            } else if (line.includes('SCOREINIT:')) {
+                scoreInit = line.split(':')[1].trim();
+                continue;
+            } else if (line.includes('SCOREDIFF:')) {
+                scoreDiff = line.split(':')[1].trim();
+                continue;
+            } else if (line.includes('BALLOON:')) {
+                const balloonData = line.split(':')[1].split(',');
+                balloons = balloonData;
+                continue;
+            }
+        }
+    }
+
+    const songData = {
+        title: title,
+        subtitle: subtitle,
+        bpm: bpm,
+        songSource: songSource,
+        offset: offset,
+        demoStart: demoStart,
+        difficulty: difficulty,
+        scoreInit: scoreInit,
+        scoreDiff: scoreDiff,
+        noteData: noteDataArray,
+    };
+
+    setSongData(songData);
+    setSongProcessed(true);
+};
+
+export default processSong;
